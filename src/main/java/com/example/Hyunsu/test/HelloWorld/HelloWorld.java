e034c767ca7690c70b13654912cabbc936627509
Cong0327 <hsp400@gmail.com>
2023-06-22 18:18:57 +0900

    test:[박현수] ci test

---
+++ src/main/java/com/example/Hyunsu/test/HelloWorld/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.example.Hyunsu.test.HelloWorld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String str = "HelloWorld!!!";
+        System.out.println(str);
+    }
+}
