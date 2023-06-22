e61c06a757ecae3fa2f62e414bba8fb1ad4d003d
Cong0327 <hsp400@gmail.com>
2023-06-22 17:25:51 +0900

    init:[박현수] proj 초기설정

---
+++ Hyunsu/src/test/java/com/example/Hyunsu/HyunsuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Hyunsu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyunsuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
