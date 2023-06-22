f160dcb471e2e708287a1244a8f79baa26ae778f
Cong0327 <hsp400@gmail.com>
2023-06-22 18:08:34 +0900

    remove:[박현수] ci test를 위한 test code 주석

--- src/test/java/com/example/Hyunsu/HyunsuApplicationTests.java
+++ src/test/java/com/example/Hyunsu/HyunsuApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class HyunsuApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
